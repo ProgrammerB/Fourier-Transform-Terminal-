ea4d26c2f3a11f59744d85e58d86cdd2b422fbb4
Braxton <lasterb@uindy.edu>
2019-03-06 15:24:12 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+@file: main.cpp
+@desc: Control file for all classes
+@date: 3/6/19
+
+Copyright(c) 2019 Braxton Laster & Ben Rader
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+  return 0;
+}
