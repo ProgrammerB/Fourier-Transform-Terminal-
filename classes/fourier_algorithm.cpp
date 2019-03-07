3bcb990df4c05eee3c34be84fae971b5e96c3ed3
Braxton Laster <lasterb@uindy.edu>
2019-03-07 13:33:18 -0500

    Add setter and getters functions to fourier class Modify Makefile.bat to include fourier.cpp

--- classes/fourier_algorithm.cpp
+++ classes/fourier_algorithm.cpp
@@ -5,3 +5,48 @@
 
 Copyright(c) 2019 Braxton Laster & Ben Rader
 */
+
+#include "Fourier.h"
+
+Fourier::Fourier(double frequency, double frequency_step)
+{
+  frequency             = new double;
+  frequency_step        = new double;
+
+  this->frequency       = frequency;
+  this->frequency_step  = frequency_step;
+}
+
+
+Fourier::~Fourier(void)
+{
+  delete frequency;
+  delete frequency_step;
+
+  frequency       = nullptr;
+  frequency_step  = nullptr;
+}
+
+
+void Fourier::setFrequency(double frequency)
+{
+  this->frequency = frequency;
+}
+
+
+void Fourier::setFrequencyStep(double frequency_step)
+{
+  this->frequency_step = frequency_step;
+}
+
+
+double Fourier::getFrequency(void) const
+{
+  return this->frequency;
+}
+
+
+double Fourier::getFrequencyStep(void) const
+{
+  return this->frequency_step;
+}
