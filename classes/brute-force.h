680a7d4c70943c4389add952d529c79dadd4e4e0
Benthediver99 <raderb@uindy.edu>
2019-04-25 22:31:53 -0400

    Style AGAIN

--- classes/brute-force.h
+++ classes/brute-force.h
@@ -8,21 +8,21 @@ using std::complex;
 
 template<typename T>
 class Brute_Force: public Fourier<T> {
-public:
-  Brute_Force();
-  Brute_Force(std::string file_name, double frequency, double frequency_step, std::string output_name);
-  ~Brute_Force();
-  //~Brute_Force();
-  std::vector<complex<double>> DFT(const std::vector<double> &index,
-    const std::vector<double> &value,std::vector<complex<double>> &result);
-private:
-  double frequency;
-  double frequency_step;
-  std::vector<complex<T>> result;
-  std::vector<T>* index;
-  std::vector<T>* value;
-  std::string file_name;
-  std::string output_name;
+  public:
+    Brute_Force();
+    Brute_Force(std::string file_name, double frequency, double frequency_step, std::string output_name);
+    ~Brute_Force();
+
+    std::vector<complex<double>> DFT(const std::vector<double> &index,
+      const std::vector<double> &value,std::vector<complex<double>> &result);
+  private:
+    double frequency;
+    double frequency_step;
+    std::vector<complex<T>> result;
+    std::vector<T>* index;
+    std::vector<T>* value;
+    std::string file_name;
+    std::string output_name;
 };
 
 #endif
