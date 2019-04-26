d672461164bbf7153e1a4747a6ac50aa2ac6ab0e
Benthediver99 <raderb@uindy.edu>
2019-04-26 19:50:52 -0400

    Fourier framework for unit testing (maybe)

---
+++ classes/fourier_test.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include <iostream>
+
+//Below is the template for our unit testing
+TEST(//function name, version of test label)
+{
+  //create some object or start brute/cooley algorithm
+  EXPECT_EQ(//some function that's getting tested, expected value);
+}
